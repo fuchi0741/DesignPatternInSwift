44faf70572f864c933b6e2b10899f89bc98d21e1
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-04-20 14:55:29 +0900

    Facadeパターン

--- Facade_Swift.playground/Contents.swift
+++ Facade_Swift.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+// Facadeパターン
+final class Defaults {
+
+    private let defaults: UserDefaults
+
+    init(defaults: UserDefaults = .standard) {
+        self.defaults = defaults
+    }
+
+    subscript(key: String) -> String? {
+        get {
+            return defaults.string(forKey: key)
+        }
+
+        set {
+            defaults.set(newValue, forKey: key)
+        }
+    }
+}
+
+// Usage
+let storage = Defaults()
+
+// Store
+storage["Bishop"] = "Disconnect me. I’d rather be nothing"
+
+// Read
+storage["Bishop"]
